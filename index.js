b2ce4f0c4f3015aeb838f6a9145dc616fab1d82a
AntonioMusolino <antoniomusolino007@gmail.com>
2017-11-23 17:30:53 +0100

    add static materialize on nodejs

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(express.static('images'));
 app.use(express.static('html'));
 app.use(express.static('css'));
 app.use(express.static('js'));
+app.use(express.static('materialize'));
 
 app.get('/',(req, res) => {
     return res.sendFile(path.join(__dirname, './index.html'))
