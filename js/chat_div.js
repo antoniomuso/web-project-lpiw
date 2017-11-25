c73be796abeb9004ca1fe8b94c0bec59824ab776
antoniomuso <antoniomusolino007@gmail.com>
2017-11-25 17:56:53 +0100

    add fadeIn

--- js/chat_div.js
+++ js/chat_div.js
@@ -1,13 +1,24 @@
-
+const fadeIn = 1000
 // prende un messaggio ricevuto dal server e lo inserisce nella chat come messaggio ricevuto 
 // da un altra persona
-function reciveMessFrom (otherName, img, message)  {
-
+function reciveMessFrom(otherName, img, message) {
+    var object = $(`<li class="other">
+        <div class="avatar">
+            <img class='cricle #c5cae9 indigo lighten-4' src="https://i.imgur.com/DY6gND0.png" draggable="false" />
+        </div>
+        <div class="msg">
+            <p>${message}</p>
+            <time>${(new Date()).toLocaleTimeString()}</time>
+        </div>
+    </li>`).hide()
+    var ol = object.appendTo('#chats').fadeIn(fadeIn)
+    return ol[0].children[ol[0].children.length - 1]
 }
 // Prende in input un nome utente un messaggio, Dovrebbe prendere anche avatar ecc...
 // restituisce il messaggio html aggiunto
-function sendMessage (name, message) {
-    var ol = $('#chats').append(`<li class="self">
+function sendMessage(name, message) {
+    var object = $(
+        `<li class="self">
         <div class="avatar">
             <img class='cricle #c5cae9 indigo lighten-4' src="https://i.imgur.com/HYcn9xO.png" draggable="false" />
         </div>
@@ -15,8 +26,9 @@ function sendMessage (name, message) {
             <p>${message}</p>
             <time>${(new Date()).toLocaleTimeString()}</time>
         </div>
-    </li>`)
-    return ol[0].children[ol[0].children.length-1]
+        </li>`).hide()
+    var ol = object.appendTo('#chats').fadeIn(fadeIn)
+    return ol[0].children[ol[0].children.length - 1]
 }
 
 
