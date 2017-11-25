3dca1d3546776cce72db8814168829a278905979
antoniomuso <antoniomusolino007@gmail.com>
2017-11-25 10:39:11 +0100

    insert routes for register

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var express = require('express')
 var fs = require('fs')
 var path = require('path')
+var bodyParser = require('body-parser')
 var port = 8000
 var app = express()
 
-
+app.use(bodyParser)
 app.use(express.static('images'));
 app.use(express.static('html'));
 app.use(express.static('css'));
 app.use(express.static('js'));
+app.use(require('./lib/register_routes'))
 
 app.get('/',(req, res) => {
     return res.sendFile(path.join(__dirname, './index.html'))
