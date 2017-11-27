d13677dbbebcb7f1925fa788b27a8d2d507610a7
antoniomuso <antoniomusolino007@gmail.com>
2017-11-27 09:40:24 +0100

    inserimento pezzo di db

---
+++ db/db_tabelle.sql
@@ -0,0 +1,52 @@
+CREATE DOMAIN smalls as varchar(20);
+CREATE DOMAIN mediums as varchar(100);
+CREATE DOMAIN bigs as varchar(1000);
+CREATE DOMAIN int_positive as Integer check (value > 0);
+CREATE DOMAIN real_positive as Real check (value > 0);
+
+CREATE TABLE Email (
+    value mediums not null,
+    primary key (value)
+);
+
+CREATE TABLE Img (
+    path mediums not null,
+    primary key (path)
+);
+
+CREATE TABLE Utente (
+    id serial not null,
+    conf boolean not null,
+    username smalls not null,
+    descr mediums not null,
+    pass varchar(40) not null,
+    sale smalls not null,
+    email mediums not null,
+    img mediums,
+    primary key (id),
+    foreign key (email) references Email(value),
+    foreign key (img) references Img(path)
+);
+
+CREATE TABLE Cookie {
+    ist timestamp not null,
+    utente integer not null,
+    scad timestamp not null,
+    foreign key utente references Utente(id),
+    check (ist < scad),
+    primary key(ist, utente)
+}
+
+CREATE TABLE Chat (
+    ist timestamp not null,
+    nome smalls not null,
+    descr mediums not null,
+    utente integer not null,
+    img mediums ,
+    scad timestamp not null,
+    primary key (ist),
+    check (ist < scad),
+    foreign key (img) references Img(path),
+    foreign key (utente) references Utente(id)
+);
+
