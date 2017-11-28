72afa48b37230c26358f8cc45914969b1909bf00
antoniomuso <antoniomusolino007@gmail.com>
2017-11-28 23:09:12 +0100

    code refactoring

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-var express = require('express')
-var fs = require('fs')
-var path = require('path')
-var bodyParser = require('body-parser')
-var { Pool } = require('pg')
-var session = require('express-session')
-var MemoryStore = require('memorystore')(session)
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const bodyParser = require('body-parser')
+const { Pool } = require('pg')
+const session = require('express-session')
+const MemoryStore = require('memorystore')(session)
+const middleware = require('./middleware/middleware')
 
-var app = express()
+const app = express()
+const client = new Pool({ connectionString: 'postgres://obbhdnav:1ONsv6xTGR21Tl2KliBMS4p5mzRrPBr1@horton.elephantsql.com:5432/obbhdnav' })
+const port = 8000
+
 app.set('view engine', 'ejs')
-const client = new Pool({
-    connectionString: 'postgres://obbhdnav:1ONsv6xTGR21Tl2KliBMS4p5mzRrPBr1@horton.elephantsql.com:5432/obbhdnav'
-})
-var port = 8000
-
 app.use(session(
     {secret: 'Nessuna la IndovinaBell',
     store: new MemoryStore({ checkPeriod: 86400000 }), resave: false, saveUninitialized: false}));
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('./'));
-app.use(require('./routes/register_routes')(client))
+
+router.use('/register',middleware.registration(client))
+router.use('/register', (err, req, res, next) => {
+    req.session.message = err.message
+    res.redirect('/')
+})
+router.post('/register', (req, res) => {
+    res.redirect('./../html/chat.html')
+})
 
 app.get('/',(req, res) => {
     return res.render('../index.ejs', {error:req.session.message}) // L'errore riguarda il login
