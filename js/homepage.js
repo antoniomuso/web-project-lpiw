7909fdce5b9d1a85f692a92165fb7ba94d09f24a
antoniomuso <antoniomusolino007@gmail.com>
2017-11-30 22:45:11 +0100

    refactoring del codice index.ejs e inserito la selezione della tabella di registrazione quando abbiamo il refresh della pagina con un errore nella registrazione

--- js/homepage.js
+++ js/homepage.js
@@ -1,75 +1,23 @@
-$("#login-tab").click(function () {
-    $("#signup-tab").css("backgroundColor", "white")
-    $("#login-tab").css("backgroundColor", "#EBEBF5")
-    $("#form-signup").css("display", "none")
-    $("#form-login").css("display", "inherit")
-    $("#login-tab").css("opacity", "1")
-    $("#signup-tab").css("opacity", "0.50")
 
-});
+$(document).ready(() => {
+    $("#login-tab").click(showLoginTab);
+    $("#signup-tab").click(showSignupTab);
+})
 
-$("#signup-tab").click(function () {
+function showSignupTab () {
     $("#signup-tab").css("backgroundColor", "#EBEBF5")
     $("#login-tab").css("backgroundColor", "white")
     $("#form-signup").css("display", "inherit")
     $("#form-login").css("display", "none")
     $("#login-tab").css("opacity", "0.50")
     $("#signup-tab").css("opacity", "1")
+} 
 
-});
-
-function addUser()
-{
-    /*$("#submitButton").click(function() {
-        
-        var errorMessage = "";
-        var fieldsMissing = "";
-        
-        if ($("#email").val() == "") {
-            
-            fieldsMissing += "<br>Email";
-            
-        }
-        
-        
-        if ($("#password").val() == "") {
-            
-            fieldsMissing += "<br>Password";
-            
-        }
-        
-        
-        if (fieldsMissing != "") {
-            
-            errorMessage += "<p>The following field(s) are missing:" + fieldsMissing;
-            
-        }
-        
-        if (isEmail($("#email").val()) == false) {
-            
-            errorMessage += "<p>Your email address is not valid</p>";
-            
-        }
-        
-        
-        if ($("#password").val() != $("#passwordConfirm").val()) {
-            
-            errorMessage += "<p>Your passwords don't match</p>";
-            
-        }
-        
-        if (errorMessage != "") {
-            
-            $("#errorMessage").html(errorMessage);
-            
-        } else {
-            
-            $("#successMessage").show();
-            $("#errorMessage").hide();
-            
-        }
-        
-    });*/
-    
-    
+function showLoginTab () {
+    $("#signup-tab").css("backgroundColor", "white")
+    $("#login-tab").css("backgroundColor", "#EBEBF5")
+    $("#form-signup").css("display", "none")
+    $("#form-login").css("display", "inherit")
+    $("#login-tab").css("opacity", "1")
+    $("#signup-tab").css("opacity", "0.50")
 }
