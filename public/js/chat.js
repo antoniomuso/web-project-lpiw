9146c74e77e4e096968a84829023e33195e8ef76
moody <jovsmnn@gmail.com>
2017-12-08 20:54:20 +0100

    barra input in fase di lavorazione

--- public/js/chat.js
+++ public/js/chat.js
@@ -280,10 +280,3 @@ $(document).mouseup(function (e) {
             });
     }
 });
-/*
-$('textarea').each(function () {
-    this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
-  }).on('input', function () {
-    this.style.height = 'auto';
-    this.style.height = (this.scrollHeight) + 'px';
-  });*/
